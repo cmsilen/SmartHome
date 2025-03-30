8121e01ac64630388d14636714f5ffe80606296a
Simone Gallo <simonecode03@gmail.com>
2025-03-30 16:17:51 +0200

    Struttura API

---
+++ src/main/java/com/example/demo/model/AddUserToBuildingRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+class addUserToBuildingRequest {
+
+    private String username;
+    private String admin;
+    private Integer buildingId;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public Integer getBuildingId() {
+        return buildingId;
+    }
+    public void setBuildingId(Integer buildingId) {
+        this.buildingId = buildingId;
+    }
+    public String getAdmin() {
+        return admin;
+    }
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+}
